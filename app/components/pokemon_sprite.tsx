3bda120d300f941f76f1e78e7e16e55bc75ca6e0
Simon Lau-Yamauchi <7174954+slauyama@users.noreply.github.com>
2026-01-29 18:58:34 -0800

    Use next/Image add github sprite url to allowlist

--- app/components/pokemon_sprite.tsx
+++ app/components/pokemon_sprite.tsx
@@ -1,10 +1,11 @@
 import { SPRITE_FRONT_DEFAULT_BASE_URL } from "../constants";
+import Image from "next/image";
 
 export function PokemonSprite({ id }: { id?: number }) {
   return (
-    <img
-      height="52px"
-      width="52px"
+    <Image
+      height={48}
+      width={48}
       alt={`pokemon sprite ${id}`}
       src={id ? `${SPRITE_FRONT_DEFAULT_BASE_URL}${id}.png` : ""}
     />
